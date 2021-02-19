ad4ba08871696d7431add403b08e32796757b5e5
Arjun Dogra <arjundogra146@gmail.com>
2021-02-19 18:40:25 +0530

    Player elements

--- src/Components/player.js
+++ src/Components/player.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import PlayCircleFilledWhiteIcon from '@material-ui/icons/PlayCircleFilledWhite';
 
 function player() {
     return (
         <div className="player">
-            <h1>player</h1>
+            <div className="player_left">
+                <h3>Playing song</h3>
+                <p>Artist name</p>
+            </div>
+            <div className="player_center">
+                <div><PlayCircleFilledWhiteIcon/></div>
+                <div>Playing</div>
+            </div>
+            <div className="player_right">
+                <div>Logo</div>
+            </div>
         </div>
     )
 }
-
 export default player
