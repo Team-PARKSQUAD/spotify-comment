d4b1f116caee61982b5a715b8fc72accf2485983
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-02-20 01:32:27 +0530

    FullPlayer intiated

--- src/Components/MainWindow.js
+++ src/Components/MainWindow.js
@@ -2,16 +2,30 @@ import React from 'react'
 import Sidebar from "./sidebar.js"
 import Player from "./player.js"
 import Albums from "./albums.js"
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function MainWindow() {
     return (
-        <div className="mainWindow">
-            <div className="container">
-                <Sidebar/>
-                <Albums/>
-            </div>
-            <Player/> 
-        </div>
+        <Router>
+            <Switch>
+
+                <Route path="/fullPlayer">
+                    <Sidebar/>
+                    <Albums/>
+                </Route>
+
+                <Route path="/"> 
+                    <div className="mainWindow">
+                        <div className="container">
+                            <Sidebar/>
+                            <Albums/>
+                        </div>
+                        <Player/> 
+                    </div>
+                </Route>
+
+            </Switch>
+        </Router>
     )
 }
 
