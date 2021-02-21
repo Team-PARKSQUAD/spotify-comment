250c7f2a8b2580084f6cf1a77cc7d0b898f4359c
Arjun Dogra <arjundogra146@gmail.com>
2021-02-21 15:18:27 +0530

    songData

--- src/Components/playing.js
+++ src/Components/playing.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import songData from "./data/songData.json";
 
 function playing(props) {
-    console.log(props)
+    console.log(songData)
+    var songInfo = {}
+    songData.map((d)=>{
+        if (d["name"] == props.match.params.song){
+            songInfo = {
+                "name": d.name,
+                "artist": d.artist,
+                "duration": d.duration,
+                "imageUrl": d.imageUrl
+            }
+        }
+    })
+    console.log(songInfo)
     return (
         <div className="playing">
-            <div>{props.match.params.song}</div>
+            <div>{songInfo.name}</div>
+            <div>{songInfo.artist}</div>
+            <div>{songInfo.duration}</div>
+            <div>{songInfo.imageUrl}</div>
         </div>
     )
 }
